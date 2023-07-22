340af91a96d243a6afb25980657eabeb88e136cf
erentasken <eren.tasken@gmail.com>
2023-07-22 18:15:56 +0300

    some refactoring on getting the keys and tokens, created new classes in order to fetch from json

--- botApp/Pages/Index.cshtml.cs
+++ botApp/Pages/Index.cshtml.cs
@@ -1,21 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Slack.Webhooks;
-using SlackNet;
-
-
-
-using JMS.UploadFile;
-using System.Runtime.CompilerServices;
-using System.ComponentModel;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SlackIntegration.Pages
 {
     public class IndexModel : PageModel
     {
-        // Replace with your own Slack webhook URL
-        private const string SlackWebhookUrl = "https://hooks.slack.com/services/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";
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
--- botApp/Pages/gptTrying.cshtml.cs
+++ botApp/Pages/gptTrying.cshtml.cs
@@ -1,22 +1,22 @@
-using System.Threading.Tasks;
+using botApp.Settings;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OpenAI_API;
 using OpenAI_API.Completions;
 using OpenAI_API.Models;
-using SlackNet.Blocks;
-using SlackNet.Events;
-using SlackNet.Interaction;
-using SlackNet.WebApi;
-using SlackNet;
-
-using SlackConnector;
 
 namespace botApp.Pages
 {
     public class gptTryingModel : PageModel
     {
-        private string APIkey = "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";
+        private string APIkey;
+        private OpenAI openAi;
+
+        public void OnGet()
+        {
+            openAi = OpenAI.GetObject();
+            APIkey = openAi.openAiKey;
+        }
 
         [BindProperty]
         public string Prompt { get; set; }
@@ -31,6 +31,7 @@ namespace botApp.Pages
                 Prompt = Prompt,
                 Model = Model.DavinciText
             };
+
             var completions = await openAI.Completions.CreateCompletionAsync(completionRequest);
 
             foreach (var completion in completions.Completions)
